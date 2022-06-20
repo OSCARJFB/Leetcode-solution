373a5aa23c49f71a981b04ee06ceded33b505123
Oscar <100116544+OSCARJFB@users.noreply.github.com>
2022-06-20 19:46:49 +0200

    Update 35. Search Insert Position.c

--- 35. Search Insert Position.c
+++ 35. Search Insert Position.c
@@ -1,8 +1,14 @@
-int searchInsert(int nums, int numsSize, int target){
+int searchInsert(int* nums, int numsSize, int target){
+    
     int i = 0;
-    for(i = 0; i  numsSize; i++) {
+    int start = numsSize / 2;
+    
+    if(nums[start] > target) start = 0;
+ 
+    for(i = start; i < numsSize; i++) {
         if(nums[i] == target) return i;
-        else if(nums[i]  target) return i; 
+        else if(nums[i] > target) return i; 
     }
+    
     return i;
 }
